e0da3aa8c07462bfdd9b82dc958d1d081b935aed
Kevin Zhang <kevin@Mac.ht.home>
2025-10-01 02:48:07 -0400

    added data helper to merge ret sample data with North America Company Name Merge by DataDate-GVKEY-IID.csv

---
+++ src/data_helpers.py
@@ -0,0 +1,28 @@
+import datetime
+import pandas as pd
+import numpy as np
+
+# make sure to use North America Company Name Merge by DataDate-GVKEY-IID.csv
+# WARNING: This creates about 2k rows of duplicates
+def merge_company_names_etc(raw, mappings):
+    mappings['datadate'] = pd.to_datetime(mappings['datadate'])
+    mappings['gvkey'] = mappings['gvkey'].astype(float)
+
+    raw['date'] = pd.to_datetime(raw['ret_eom'])
+
+    merged = pd.merge(raw, mappings3[['gvkey', 'datadate', 'iid', 'tic', 'conm', 'cusip','cik']], 
+                    left_on=['gvkey', 'date', 'iid'], 
+                    right_on=['gvkey', 'datadate', 'iid'], 
+                    how='left')
+    
+    # checking duplicates for your info
+    # Check for duplicates in the merged DataFrame
+    original_row_count = raw.shape[0]
+    merged_row_count = merged.shape[0]
+
+    if original_row_count == merged_row_count:
+        print("No duplicates were created during the merge.")
+    else:
+        print(f"Duplicates detected: Original rows = {original_row_count}, Merged rows = {merged_row_count}, Difference = {merged_row_count - original_row_count}")
+
+    return merged
